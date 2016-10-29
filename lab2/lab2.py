4c5533d0a8998badf67677ed24151be70e3f34ea
adowsky <segodj112@gmail.com>
2016-10-29 18:53:32 +0200

    hsv2 rgb fix and hsv gradients impl

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import division             # Division in Python 2.7
+from __future__ import division  # Division in Python 2.7
 import matplotlib
-matplotlib.use('Agg')                       # So that we can render files without GUI
+
+matplotlib.use('Agg')  # So that we can render files without GUI
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import numpy as np
 
 from matplotlib import colors
 
-    
+
 def plot_color_gradients(gradients, names):
     # For pretty latex fonts (commented out, because it does not work on some machines)
     # rc('text', usetex=True)
     # rc('font', family='serif', serif=['Times'], size=10)
     rc('legend', fontsize=10)
 
-    column_width_pt = 400         # Show in latex using \the\linewidth
+    column_width_pt = 400  # Show in latex using \the\linewidth
     pt_per_inch = 72
     size = column_width_pt / pt_per_inch
 
@@ -35,7 +36,7 @@ def plot_color_gradients(gradients, names):
 
         pos = list(ax.get_position().bounds)
         x_text = pos[0] - 0.25
-        y_text = pos[1] + pos[3]/2.
+        y_text = pos[1] + pos[3] / 2.
         fig.text(x_text, y_text, name, va='center', ha='left', fontsize=10)
 
     fig.savefig('my-gradients.pdf')
@@ -49,28 +50,31 @@ def hsv2rgb(h, s, v):
     elif h <= 60:
         b = (1 - s) * v
         r = v
-        g = (1 - s) * v + h/60 * v
+        g = b + h / 60 * s * v
     elif h <= 120:
         b = (1 - s) * v
         g = v
-        r = ((120 - h)/60) * v
+        r = b + ((120 - h) / 60) * s * v
+        o = 0
     elif h <= 180:
         r = (1 - s) * v
         g = v
-        b = (1 - s) * v + (h - 120)/60 * v
+        b = r + (h - 120) / 60 * s * v
 
     elif h <= 240:
         r = (1 - s) * v
         b = v
-        g = (1. - s) * v + ((240 - h)/60) * v
+        g = r + ((240 - h) / 60) * s * v
+        o = 0
     elif h <= 300:
         g = (1 - s) * v
         b = v
-        r = (1 - s) * v + (h - 240)/60 * v
+        r = g + (h - 240) / 60 * s * v
+        o = 0
     else:
         g = (1 - s) * v
         r = v
-        b = (1 - s) * v + ((360 - h)/60) * v
+        b = g + ((360 - h) / 60) * s * v
 
     return r, g, b
 
@@ -81,64 +85,61 @@ def gradient_rgb_bw(v):
 
 def gradient_rgb_gbr(v):
     if v > 0.5:
-        v2 = (1 - v)*2
+        v2 = (1 - v) * 2
     else:
-        v2 = v*2
-    return max(0, (v - 0.5)*2), max(0, (0.5 - v)*2), v2
+        v2 = v * 2
+    return max(0, (v - 0.5) * 2), max(0, (0.5 - v) * 2), v2
 
 
 def gradient_rgb_gbr_full(v):
     if v <= 0.5:
-        b = v*4
+        b = v * 4
     else:
-        b = (1-v)*4
-    return min(1, max((4*v - 2), 0)), min(1, max((-4*v + 2), 0)), min(1, b)
+        b = (1 - v) * 4
+    return min(1, max((4 * v - 2), 0)), min(1, max((-4 * v + 2), 0)), min(1, b)
 
 
 def gradient_rgb_wb_custom(v):
-    if v <= 3/7:
-        r = max(0, min(1, -7*v + 2))
-    elif v <= 10/14:
-        r = max(0, min(1, 7*v - 4))
+    if v <= 3 / 7:
+        r = max(0, min(1, -7 * v + 2))
+    elif v <= 10 / 14:
+        r = max(0, min(1, 7 * v - 4))
     else:
-        r = max(0, min(1, -7*v + 7))
+        r = max(0, min(1, -7 * v + 7))
 
-    #green
-    if v <= 2/7:
-        g = max(0, -7*v + 1)
-    elif v <= 4/7:
-        g = max(0, min(1, 7*v - 2))
+    # green
+    if v <= 2 / 7:
+        g = max(0, -7 * v + 1)
+    elif v <= 4 / 7:
+        g = max(0, min(1, 7 * v - 2))
     else:
-        g = max(0, min(1, -7*v + 6))
+        g = max(0, min(1, -7 * v + 6))
 
-    return r, g, max(0, min(1, -7*v + 4))
+    return r, g, max(0, min(1, -7 * v + 4))
 
 
 def gradient_hsv_bw(v):
-    
-    return hsv2rgb(v*360, 1, 1)
+    return hsv2rgb(0, 0, v)
 
 
 def gradient_hsv_gbr(v):
-    #TODO
-    return hsv2rgb(0, 0, 0)
+    return hsv2rgb(120 + v * 240, 1, 1)
+
 
 def gradient_hsv_unknown(v):
-    #TODO
-    return hsv2rgb(v, 0, 0)
+    return hsv2rgb(120 - v * 120, 0.5, 1)
 
 
 def gradient_hsv_custom(v):
-    #TODO
-    return hsv2rgb(0, 0, 0)
+    return hsv2rgb(v*360, 1 - v, 1)
 
 
 if __name__ == '__main__':
     def toname(g):
         return g.__name__.replace('gradient_', '').replace('_', '-').upper()
 
+
     gradients = (gradient_rgb_bw, gradient_rgb_gbr, gradient_rgb_gbr_full, gradient_rgb_wb_custom,
                  gradient_hsv_bw, gradient_hsv_gbr, gradient_hsv_unknown, gradient_hsv_custom)
 
     plot_color_gradients(gradients, [toname(g) for g in gradients])
-
